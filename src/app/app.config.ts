f80c0d68926fe6f4c1abdc000bfcb2b8f3e57e9e
Dorota1997 <gildota@wp.pl>
2025-05-10 15:00:27 +0200

    Format files

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,5 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-  ],
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)],
 };
