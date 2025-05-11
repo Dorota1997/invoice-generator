74a02733aabbebc6a2db819adec350a5eb75a365
Dorota Gil <gildota@wp.pl>
2025-05-11 20:54:15 +0200

    feature/create invoice service (#8)
    * Create invoice item interface
    
    * Create service with set and get items method

---
+++ src/app/services/invoice.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+import { InvoiceItem } from '../models/invoice-item.model';
+
+@Injectable({ providedIn: 'root' })
+export class InvoiceService {
+  private itemsSubject = new BehaviorSubject<InvoiceItem[]>([]);
+  items$ = this.itemsSubject.asObservable();
+
+  setItems(items: InvoiceItem[]): void {
+    this.itemsSubject.next(items);
+  }
+
+  getItems(): InvoiceItem[] {
+    return this.itemsSubject.value;
+  }
+}
