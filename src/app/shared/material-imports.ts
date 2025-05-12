b2a78022a9592e620eec3a6f6e305332f81199f3
Dorota Gil <gildota@wp.pl>
2025-05-12 15:10:44 +0200

    feature/create menu component (#14)
    * Export mat toolbar module
    
    * Create menu with two buttons
    
    * Use app-menu

--- src/app/shared/material-imports.ts
+++ src/app/shared/material-imports.ts
@@ -4,6 +4,7 @@ import { MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
@@ -16,4 +17,5 @@ export const materialModules = [
   MatDividerModule,
   MatProgressSpinnerModule,
   MatTableModule,
+  MatToolbarModule,
 ];
